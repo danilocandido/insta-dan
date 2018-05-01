fdd431696bd51ed32c19226d64de0bc8695da88f
Danilo Cândido <danilod07@gmail.com>
2018-05-01 18:26:34 -0300

    router and view Login in InstaDan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './components/Login';
+import {Router, Route, browserHistory} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
 import './css/reset.css'
@@ -8,7 +10,10 @@ import './css/timeline.css'
 import './css/login.css'
 
 ReactDOM.render(
-  <App />, 
+  (<Router history={browserHistory}>
+    <Route path="/" component={Login}></Route>
+    <Route path="/timeline" component={App}></Route>
+  </Router>), 
   document.getElementById('root')
 );
 registerServiceWorker();
